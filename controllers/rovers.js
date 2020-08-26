95e3542514ec7ade8eed7f47e0407929bcec6d45
YardenLawson <yardenlawson@gmail.com>
2020-08-27 00:42:47 +0100

    controllers update + middleware + async.js

--- controllers/rovers.js
+++ controllers/rovers.js
@@ -1,6 +1,7 @@
 const Rover = require('../models/rover')
 const { notFound } = require('../lib/errorMessages')
-const { findByIdAndUpdate } = require('../models/rover')
+const asyncHandler = require('../middleware/async')
+// const { findByIdAndUpdate } = require('../models/rover')
 // const { notFound, unauthorized } = require('../lib/errorMessages')
 
 // * Create the controllers for your resouce here (index, create), (show, update delete optional)
@@ -15,24 +16,39 @@ async function roversIndex(req, res, next) {
   }
 }
 
-async function roversCreate(req, res, next) {
-  try {
-    const createdRover = await Rover.create(req.body)
-    //* NEED TO CAPITALIZE NESW
-    //* NEED TO CHECK BODY CONTAINS X, Y, POSITION
-    //* OBJECT.KEYS
-    //* INVALID IF COORDINATES ARE OUTSIDE OF THE 5*5 GRID
-    res.status(201).json(createdRover)
-  } catch (err) {
-    next(err)
+const roversCreate = asyncHandler(async(req, res, next) =>  {
+  //* checking to see if req.body contains positions x & y & position or if it is undefined
+  if (!req.body.x || req.body.x === undefined) {
+    res.status(400).json({ status: 'failed', error: 'missing position x' })
   }
-}
+  if (!req.body.y || req.body.y === undefined) {
+    res.status(400).json({ status: 'failed', error: 'missing position y' })
+  }
+  if (!req.body.position || req.body.position === undefined) {
+    res.status(400).json({ status: 'failed', error: 'missing rover facing position' })
+  } 
+
+  const newRover = {
+    //* turning x and y into ints and turning position to upperCase
+    x: parseInt(req.body.x),
+    y: parseInt(req.body.y),
+    position: req.body.position.toUpperCase() 
+  }
+
+  const createdRover = await Rover.create(newRover) //* Using the modified rover object
+  //* NEED TO CAPITALIZE 'N''E''S''W'
+  //* NEED TO CHECK BODY CONTAINS X, Y, POSITION
+  //* OBJECT.KEYS
+  //* INVALID IF COORDINATES ARE OUTSIDE OF THE 5*5 GRID
+  res.status(201).json(createdRover)
+  
+})
 
 async function roversShow(req, res, next) {
   //* this id is the object id
   //* whatever goes into /id: is referred to as the req.params.id
   const roverId = req.params.id
-  //* if there's a valid mongo id but it's not a 'currently valid' one it will still error now. Forces it to go down to catch block. Throwing an error works in the same way as return so it shortcuts the circut. THROWING TO CATCH
+  //* if there's a valid mongo id but it's not a 'currently valid' one it will still error now. Forces it to go down to catch block. Throwing an error works in the same way as return so it shortcuts the circut. "THROWING TO CATCH"
   try {
     const rover = await Rover.findById(roverId)
     if (!rover) throw new Error(notFound)
@@ -43,39 +59,38 @@ async function roversShow(req, res, next) {
 }
 
 async function roversMovement(req, res, next) {
-  if (!req.body.id || !req.body.movement) throw new Error(notFound)
+  // if (!req.body.id || !req.body.movement) throw new Error(notFound)
 
-  const roverId = req.body.id
-  const movementsArray = req.body.movement.split('') //* splits the movement string to array
-  try {
-    // * find the rover to be moved
-    const rover = await Rover.findById(roverId)
-    if (!rover) throw new Error(notFound)
-    Object.assign(rover, req.body) // * merge the objects together to make the update
-    await rover.save() // * then resave
-    // * if not then send them back an unauthorised response
-    res.status(202).json(rover)
-  } catch (err) {
-    next(err)
-  }
-  if (movementsArray.toUpperCase().includes('L' || 'R' || 'M')) {
-    const rover = await Rover.findById(roverId)
-    const roverInMovement = { x: rover.x, y: rover.y, position: rover.position }
-    let i
-    for (i = 0; i < movementsArray.length; i++) {
+  // const roverId = req.body.id 
+  // const movementsArray = req.body.movement.toUpperCase().split('') //* turns the movement string to uppercase then splits the movement string to an array
+  // try {
+  //   // * find the rover to be moved
+  //   const rover = await Rover.findById(roverId)
+  //   if (!rover) throw new Error(notFound)
+  //   Object.assign(rover, req.body) // * merge the objects together to make the update
+  //   await rover.save() // * then resave
+  //   // * if not then send them back an unauthorised response
+  //   res.status(202).json(rover)
+  // } catch (err) {
+  //   next(err)
+  // }
+  // if (movementsArray.includes('L' || 'R' || 'M')) {
+  //   const roverInMovement = { x: rover.x, y: rover.y, position: rover.position }
+  //   const movementOptions = { N: { L: 'E', R: 'W', M: 'y+1' }, E: { L: 'S', R: 'N', M: 'x-1' }, S: { L: 'W', R: 'E', M: 'y-1' }, W: { L: 'S', R: 'N', M: 'x+1' } }
+  //   let i
+  //   for (i = 0; i < movementsArray.length; i++) {
       
-    }
-    findByIdAndUpdate()
-    const movementOptions = { N: { L: 'E', R: 'W', M: 'y+1' }, E: { L: 'S', R: 'N', M: 'x-1' }, S: { L: 'W', R: 'E', M: 'y-1' }, W: { L: 'S', R: 'N', M: 'x+1' } }
-  }
-  //* do these movements !include 'L, M, R,' -> ERROR 
-  //* IF YES THEN CONTINUE TO LOOP
-  //* for loop iterating over array.length to get movement
-  //* need immutable copy of rover that is found by id -> let roverInMovement = {x: rover.x, y: rover.y, position: rover.position}
-  //* at the end of the loop findByIdAndUpdate -> this will change the position of the rover
-  //* to imitate movement need to return array of positions -> each time I iterate of movement array -> pushed into position array. The rover needs to display:none from previous position and only appear in the new position
-  //* const movementOptions = {N: {L: 'E', R: 'W', M:'y+1'}, E:{L: 'S', R: 'N', M:'x-1'}, S:{L:'W', R:'E', M:'y-1'}, W:{L:'S', R:'N',  x+1}}
+  //   }
+  //   findByIdAndUpdate()
+  // }
 }
+//* do these movements !include 'L, M, R,' -> ERROR 
+//* IF YES THEN CONTINUE TO LOOP
+//* for loop iterating over array.length to get movement
+//* need immutable copy of rover that is found by id -> let roverInMovement = {x: rover.x, y: rover.y, position: rover.position}
+//* at the end of the loop findByIdAndUpdate -> this will change the position of the rover
+//* to imitate movement need to return array of positions -> each time I iterate of movement array -> pushed into position array. The rover needs to display:none from previous position and only appear in the new position
+//* const movementOptions = {N: {L: 'E', R: 'W', M:'y+1'}, E:{L: 'S', R: 'N', M:'x-1'}, S:{L:'W', R:'E', M:'y-1'}, W:{L:'S', R:'N',  'x+1'}}
 
 async function roversDelete(req, res, next) {
   const roverId = req.params.id
@@ -95,6 +110,6 @@ module.exports = {
   index: roversIndex,
   create: roversCreate,
   show: roversShow,
-  update: roversMovement,
+  movement: roversMovement,
   delete: roversDelete
 }
--- index.js
+++ index.js
@@ -19,4 +19,11 @@ app.use(errorHandler)
 
 const PORT = process.env.PORT || 8000 //* this is a fallback incase the process.env file doesn't work
 
-app.listen(PORT, () => console.log(`Express is listening on port ${PORT}`))
+const server = app.listen(PORT, () => console.log(`Express is listening on port ${PORT}`))
+
+//* handle unhandled promise rejections: (node:44800) [DEP0018] DeprecationWarning: Unhandled promise rejections are deprecated.
+//* kills the server
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`err: ${err.message}`)
+  server.close(() => process.exit(1))
+})
